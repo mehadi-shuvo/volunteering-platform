6103e5d428c0bab94d4cb663834df155698826f3
mehadi-shuvo <mehadihasanshuvo88@gmail.com>
2025-03-17 16:27:03 +0600

    feat: add create event modal

--- client/src/app/pages/event/Events.tsx
+++ client/src/app/pages/event/Events.tsx
@@ -1,25 +1,16 @@
 import { useState, useEffect } from "react";
 import { getEventsApi } from "../../../apis/auth/eventsApi";
-import { TUser } from "../../../utils/types/types";
-
-interface Event {
-  id: number;
-  title: string;
-  description: string;
-  date: string;
-  time: string;
-  location: string;
-  category: string;
-  attendees: TUser[];
-  organizer_id: string;
-}
+import EventCard from "../../../components/EventCard";
+import { TEvent } from "../../../utils/types/types";
+import CreateEventModal from "../../../components/CreateEventModal";
 
 const Events = () => {
-  const [events, setEvents] = useState<Event[]>([]);
+  const [events, setEvents] = useState<TEvent[]>([]);
   const [searchTerm, setSearchTerm] = useState("");
   const [selectedCategory, setSelectedCategory] = useState("");
   const [selectedLocation, setSelectedLocation] = useState("");
   const [selectedDate, setSelectedDate] = useState("");
+  // const [isModalOpen, setIsModalOpen] = useState(false);
 
   // Fetch events with filters
   const fetchEvents = async () => {
@@ -46,10 +37,6 @@ const Events = () => {
     event.title.toLowerCase().includes(searchTerm.toLowerCase())
   );
 
-  const handleJoinEvent = (id: string) => {
-    console.log({ join: id });
-  };
-
   return (
     <div className="min-h-screen bg-white p-6">
       <div className="max-w-6xl mx-auto">
@@ -62,7 +49,14 @@ const Events = () => {
             Find upcoming events, register with one click, and make a difference
             in your community!
           </p>
-          <button className="secondary-bg mt-4 px-6 py-2 rounded-lg text-white font-bold hover:bg-yellow-500 transition-all">
+          <button
+            className="secondary-bg mt-4 px-6 py-2 rounded-lg text-white font-bold hover:bg-yellow-500 transition-all"
+            onClick={() =>
+              (
+                document.getElementById("my_modal_3") as HTMLDialogElement
+              ).showModal()
+            }
+          >
             Create Event
           </button>
         </div>
@@ -107,87 +101,19 @@ const Events = () => {
         {/* Event Listing */}
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
           {filteredEvents.map((event) => (
-            <div
-              key={event.id}
-              className="primary-bg text-white rounded-lg shadow-md p-6 hover:shadow-lg transition-all"
-            >
-              <h2 className="text-2xl font-bold mb-2">{event.title}</h2>
-              <p className="text-sm mb-4">{event.description}</p>
-              <div className="flex flex-col gap-2 mb-4">
-                {/* Date & Time */}
-                <div className="flex items-center gap-2 text-gray-300">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    strokeWidth={1.5}
-                    stroke="currentColor"
-                    className="size-5 "
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 0 1 2.25-2.25h13.5A2.25 2.25 0 0 1 21 7.5v11.25m-18 0A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75m-18 0v-7.5A2.25 2.25 0 0 1 5.25 9h13.5A2.25 2.25 0 0 1 21 11.25v7.5"
-                    />
-                  </svg>
-                  <span className="">
-                    {event.date} | {event.time}
-                  </span>
-                </div>
-
-                {/* Location */}
-                <div className="flex items-center gap-2 text-gray-300">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    strokeWidth={1.5}
-                    stroke="currentColor"
-                    className="size-5 "
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      d="M15 10.5a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
-                    />
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1 1 15 0Z"
-                    />
-                  </svg>
-                  <span className="">{event.location}</span>
-                </div>
-
-                {/* Attendees */}
-                <div className="flex items-center gap-2 text-gray-300">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    strokeWidth={1.5}
-                    stroke="currentColor"
-                    className="size-5 "
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      d="M18 18.72a9.094 9.094 0 0 0 3.741-.479 3 3 0 0 0-4.682-2.72m.94 3.198.001.031c0 .225-.012.447-.037.666A11.944 11.944 0 0 1 12 21c-2.17 0-4.207-.576-5.963-1.584A6.062 6.062 0 0 1 6 18.719m12 0a5.971 5.971 0 0 0-.941-3.197m0 0A5.995 5.995 0 0 0 12 12.75a5.995 5.995 0 0 0-5.058 2.772m0 0a3 3 0 0 0-4.681 2.72 8.986 8.986 0 0 0 3.74.477m.94-3.197a5.971 5.971 0 0 0-.94 3.197M15 6.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Zm6 3a2.25 2.25 0 1 1-4.5 0 2.25 2.25 0 0 1 4.5 0Zm-13.5 0a2.25 2.25 0 1 1-4.5 0 2.25 2.25 0 0 1 4.5 0Z"
-                    />
-                  </svg>
-                  <span className="">{event.attendees.length} Attendees</span>
-                </div>
-              </div>
-              <button
-                onClick={() => handleJoinEvent("dsdfdfdf")}
-                className="secondary-bg w-full py-2 rounded-lg text-white font-bold hover:bg-yellow-500 transition-all"
-              >
-                Join Event
-              </button>
-            </div>
+            <EventCard key={event.id} event={event}></EventCard>
           ))}
         </div>
       </div>
+
+      {/* Modal */}
+
+      <dialog id="my_modal_3" className="modal">
+        <CreateEventModal userId="opp" />
+        <form method="dialog" className="modal-backdrop">
+          <button>close</button>
+        </form>
+      </dialog>
     </div>
   );
 };
---
+++ client/src/components/CreateEventModal.tsx
@@ -0,0 +1,138 @@
+import { useForm, SubmitHandler } from "react-hook-form";
+import { TEvent } from "../utils/types/types";
+
+interface CreateEventModalProps {
+  userId: string;
+}
+
+const CreateEventModal = ({ userId }: CreateEventModalProps) => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<TEvent>();
+
+  const onSubmit: SubmitHandler<TEvent> = (data) => {
+    console.log({ data, userId });
+    // Here you would typically make an API call to create the event
+    // setIsModalOpen(false); // Close the modal after submission
+  };
+
+  return (
+    <div className="fixed inset-0 flex items-center justify-center  bg-opacity-50 backdrop-blur-sm p-5 overflow-auto">
+      <div className="bg-white p-8 rounded-2xl shadow-2xl max-h-[90vh] overflow-y-auto w-full max-w-md md:max-w-4xl transform transition-all duration-300 scale-95 hover:scale-100">
+        <h2 className="text-2xl font-bold primary-text mb-6 text-center">
+          Create Event
+        </h2>
+        <form onSubmit={handleSubmit(onSubmit)} className="space-y-5">
+          <div>
+            <label className="block text-lg font-semibold secondary-text mb-2">
+              Event Title
+            </label>
+            <input
+              {...register("title", { required: "Title is required" })}
+              type="text"
+              className="w-full p-3 border border-gray-200 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+            />
+            {errors.title && (
+              <p className="text-red-500 text-sm mt-1">
+                {errors.title.message}
+              </p>
+            )}
+          </div>
+          <div>
+            <label className="block text-lg font-semibold secondary-text mb-2">
+              Description
+            </label>
+            <textarea
+              {...register("description", {
+                required: "Description is required",
+              })}
+              className="w-full p-3 border border-gray-200 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+            />
+            {errors.description && (
+              <p className="text-red-500 text-sm mt-1">
+                {errors.description.message}
+              </p>
+            )}
+          </div>
+          <div>
+            <label className="block text-lg font-semibold secondary-text mb-2">
+              Category
+            </label>
+            <select
+              {...register("category", { required: "Category is required" })}
+              className="w-full p-3 border border-gray-200 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+            >
+              <option value="">Select Category</option>
+              <option value="Environment">Environment</option>
+              <option value="Social Welfare">Social Welfare</option>
+              <option value="Education">Education</option>
+            </select>
+            {errors.category && (
+              <p className="text-red-500 text-sm mt-1">
+                {errors.category.message}
+              </p>
+            )}
+          </div>
+          <div>
+            <label className="block text-lg font-semibold secondary-text mb-2">
+              Location
+            </label>
+            <input
+              {...register("location", { required: "Location is required" })}
+              type="text"
+              className="w-full p-3 border border-gray-200 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+            />
+            {errors.location && (
+              <p className="text-red-500 text-sm mt-1">
+                {errors.location.message}
+              </p>
+            )}
+          </div>
+          <div>
+            <label className="block text-lg font-semibold secondary-text mb-2">
+              Date
+            </label>
+            <input
+              {...register("date", { required: "Date is required" })}
+              type="date"
+              className="w-full p-3 border border-gray-200 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+            />
+            {errors.time && (
+              <p className="text-red-500 text-sm mt-1">{errors.time.message}</p>
+            )}
+          </div>
+          <div>
+            <label className="block text-lg font-semibold secondary-text mb-2">
+              Time
+            </label>
+            <input
+              {...register("time", { required: "Time is required" })}
+              type="time"
+              className="w-full p-3 border border-gray-200 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+            />
+            {errors.date && (
+              <p className="text-red-500 text-sm mt-1">{errors.date.message}</p>
+            )}
+          </div>
+          <div className="mt-8 flex justify-end space-x-4">
+            <form method="dialog">
+              <button className="btn secondary-bg text-white px-6 py-2 rounded-lg font-semibold hover:bg-yellow-500 transition-all duration-300 transform hover:scale-105">
+                Close
+              </button>
+            </form>
+            <button
+              type="submit"
+              className="primary-bg text-white px-6 py-2 rounded-lg font-semibold hover:bg-blue-600 transition-all duration-300 transform hover:scale-105"
+            >
+              Create Event
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default CreateEventModal;
