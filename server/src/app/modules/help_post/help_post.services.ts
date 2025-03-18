4e4d6f596014705e12d0736e4de2bd286858dd48
mehadi-shuvo <mehadihasanshuvo88@gmail.com>
2025-03-18 16:57:29 +0600

    feat: add post's comment api

--- server/src/app/modules/help_post/help_post.services.ts
+++ server/src/app/modules/help_post/help_post.services.ts
@@ -1,4 +1,4 @@
-import { HelpPost } from "@prisma/client";
+import { Comments, HelpPost } from "@prisma/client";
 import prismaC from "../../../utils/prismaClient";
 
 const createHelpPost = async (payload: HelpPost) => {
@@ -20,7 +20,18 @@ const createHelpPost = async (payload: HelpPost) => {
 
 const getHelpPosts = async (): Promise<HelpPost[] | []> => {
   const result = await prismaC.helpPost.findMany({
-    include: { comments: true }, // Include comments in the response
+    include: { comments: true, user: true },
+  });
+  return result;
+};
+
+const addComment = async (data: {
+  comment: string;
+  user_id: string;
+  post_id: string;
+}): Promise<Comments> => {
+  const result = await prismaC.comments.create({
+    data,
   });
   return result;
 };
@@ -28,4 +39,5 @@ const getHelpPosts = async (): Promise<HelpPost[] | []> => {
 export const helpPostServices = {
   createHelpPost,
   getHelpPosts,
+  addComment,
 };
